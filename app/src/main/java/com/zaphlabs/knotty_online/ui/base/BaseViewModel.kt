22078e7d7319fbec160b248b65245130e1f7ec97
mohammad.zaki <arshu786>
2019-08-27 11:19:50 +0530

    Fixes

--- app/src/main/java/com/zaphlabs/knotty_online/ui/base/BaseViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.zaphlabs.knotty_online.ui.base
-
-import androidx.appcompat.app.AppCompatActivity
-
-class BaseViewModel : AppCompatActivity() {
-}
---
+++ app/src/main/kotlin/com/zaphlabs/knotty_online/ui/base/BaseViewModel.kt
@@ -0,0 +1,7 @@
+package com.zaphlabs.knotty_online.ui.base
+
+import androidx.lifecycle.ViewModel
+
+class BaseViewModel : ViewModel() {
+
+}
